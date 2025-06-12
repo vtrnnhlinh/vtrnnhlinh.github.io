788ad3ad569e470a03cb2f3f358b02c4228e2801
vtrnnhlinh <vtrnnhlinh@gmail.com>
2025-06-12 08:44:06 +0000

    Deploying to gh-pages from @ vtrnnhlinh/vtrnnhlinh.github.io@f0bd09c900df2a012c3411e442c0558c08c564e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -413,12 +413,7 @@ ninja.data = [{
           section: "News",},{id: "news-submit-the-hardcover-version-of-my-thesis-to-the-faculty",
           title: 'Submit the hardcover version of my Thesis to the Faculty',
           description: "",
-          section: "News",},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
+          section: "News",},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
