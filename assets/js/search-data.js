5e06dc349ece50c42c7db32e447f6b7354251fac
vtrnnhlinh <vtrnnhlinh@gmail.com>
2025-06-12 03:45:31 +0000

    Deploying to gh-pages from @ vtrnnhlinh/vtrnnhlinh.github.io@a6d65735919905f31828d6c08a34af6b54405a63 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,13 +443,11 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-start-my-internship-at-vierobot",
+          section: "News",},{id: "news-start-my-internship-at-vierobot",
           title: 'Start my internship at Vierobot',
+          description: "",
+          section: "News",},{id: "news-join-hpqc-team-hcmut",
+          title: 'Join HPQC Team - HCMUT',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
