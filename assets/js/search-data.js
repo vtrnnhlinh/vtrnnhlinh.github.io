58ac45f3a21f3da5668e5c4b96e21ece7b6350b3
vtrnnhlinh <vtrnnhlinh@gmail.com>
2026-03-30 07:20:06 +0000

    Deploying to gh-pages from @ vtrnnhlinh/vtrnnhlinh.github.io@6fa38a78a3120513e3adcae2cdaf0514c3db1268 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-jan-2026-reading-log",
+        },{id: "post-mar-2026-reading-log",
+        
+          title: "Mar 2026 Reading Log",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/mar-26-reading-log/";
+          
+        },
+      },{id: "post-jan-2026-reading-log",
         
           title: "Jan 2026 Reading Log",
         
